c0b03c71b3f808f5e4d17e87b3745bf7f43c06d9
tomascript <tomas.ko@centrum.sk>
2022-08-01 21:01:29 +0200

    changes

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-// app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.json());
+// app.use(express.static('public'));
+app.set('view engine', 'ejs');
 // app.set('views', './views')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res)=>{
-   res.send('GET to Home Page')
-})
+app.get('/', (req, res) => {
+  res.send('GET to Home Page');
+});
 
-app.post('/:val', (req, res)=>{
+app.post('/:val', (req, res) => {
+  const param = req.params.val;
+  let value = req.body instanceof Array ? req.body[0][param] : req.body[param];
 
-    const param = req.params.val
-    let value = req.body instanceof Array ? req.body[0][param] : req.body[param]
+  if (value) {
+    res.render('pages/index', { show: value });
+  } else {
+    res.send(`${param} does not exist in posted json as key.`);
+  }
+});
 
-    if (value){
-        res.render('pages/index', {show: value})   
-    } else {
-        res.send(`${param} does not exist in posted json as key.`)
-    }
-})
+app.post('/', (req, res) => res.send('No parameter name after in URL after /'));
 
-app.post('/', (req,res)=>res.send('No parameter name after in URL after /'))
+app.get('*', (req, res) => res.redirect('/'));
 
-app.get('*', res.redirect('/'))
-
-
-app.listen(PORT, ()=>console.log('server started'))
+app.listen(PORT, () => console.log('server started'));
